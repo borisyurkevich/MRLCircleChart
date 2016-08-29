3c5e4a208b18ae9a9f5331fb1e9af761f8e6f72c
mlisik <lisik.marek@gmail.com>
2016-08-29 22:34:41 +0200

    updates demo viewcontroller setup and removes generic constraint from simple data source

--- Source/SimpleChartDataSource.swift
+++ Source/SimpleChartDataSource.swift
@@ -8,21 +8,11 @@
 
 import Foundation
 
-/**
- Dummy protocol that groups common numeric types
- */
-public protocol Number {}
-
-extension Float: Number {}
-extension Double: Number {}
-extension Int: Number {}
-extension UInt: Number {}
-extension CGFloat: Number {}
 
 /**
  Convenience implementation of `ChartDataSource` that can be initialized with a `[Number]` and will create segments
  */
-public class NumberChartDataSource<T where T: Number>: ChartDataSource {
+public class NumberChartDataSource: ChartDataSource {
   public var segments: [ChartSegment]
   public var maxValue: Double
   
@@ -32,12 +22,9 @@ public class NumberChartDataSource<T where T: Number>: ChartDataSource {
    - parameter items: an array of `Number`-conforming numeric types
    - parameter maxValue: the initial maximum reference value for the chart segments angle calculations
    */
-  public init(items: [T], maxValue: Double) {
-    self.segments = items.flatMap {
-      guard let number = $0 as? Double else {
-        return nil
-      }
-      return ChartSegment(value: number, description: "value: \(number)")
+  public init(items: [Double], maxValue: Double) {
+    self.segments = items.map {
+      return ChartSegment(value: $0, description: "$0")
     }
     self.maxValue = maxValue
   }
